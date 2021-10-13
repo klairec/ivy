7267499bd65e48064f6704b35a2d122d3f39a72a
klairec <claire.krucker@hotmail.fr>
2021-10-13 20:39:57 +0200

    add rel="noreferrer"

--- src/pages/index.js
+++ src/pages/index.js
@@ -16,7 +16,7 @@ const IndexPage = () => {
         <h1 class="title">IVY <span>✌️</span></h1>
       </div>
       <div class="bio">
-        <p>Amatrice d’ambiances lourdes et breakées, <b>IVY</b> explore quotidiennement et avec exigence les tréfonds des musiques électroniques tout genre confondu pour n’en ressortir que ce qui la fait vibrer au plus au point. Membre du collectif <a href="https://www.facebook.com/Breakboostaaa" target="_blank"><b>BreakBoost</b></a> à Rennes, aux côtés de ses frères d’armes <a href="https://soundcloud.com/schlossc" target="_blank"><b>Schloss</b></a> et <a href="https://soundcloud.com/mi-ntt" target="_blank"><b>MI:NT</b></a>, elle trouve sa famille de coeur et la volonté de partager ses trésors musicaux.<br/>
+        <p>Amatrice d’ambiances lourdes et breakées, <b>IVY</b> explore quotidiennement et avec exigence les tréfonds des musiques électroniques tout genre confondu pour n’en ressortir que ce qui la fait vibrer au plus au point. Membre du collectif <a href="https://www.facebook.com/Breakboostaaa" target="_blank" rel="noreferrer"><b>BreakBoost</b></a> à Rennes, aux côtés de ses frères d’armes <a href="https://soundcloud.com/schlossc" target="_blank" rel="noreferrer"><b>Schloss</b></a> et <a href="https://soundcloud.com/mi-ntt" target="_blank" rel="noreferrer"><b>MI:NT</b></a>, elle trouve sa famille de coeur et la volonté de partager ses trésors musicaux.<br/>
         Les yeux rivés sur les scènes émergentes du globe, elle fait constamment évoluer ses influences pour ne jamais se lasser ou bien encore pour assouvir sa soif d’expériences sonores.</p>
       </div>
       <div class="podcasts">

3d0ffc6f0d53bd33552b2d74e3a659e96662cd82
klairec <claire.krucker@hotmail.fr>
2021-10-13 20:03:23 +0200

    improve index

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,15 +1,46 @@
-// Step 1: Import React
 import * as React from 'react'
-import Layout from '../components/layout'
+import './index.css'
 import { Link } from 'gatsby'
 
-// Step 2: Define your component
+import electroLabCover from '../images/electroLab.png'
+import moduleCover from '../images/module.png'
+import rinsePEEVCover from '../images/rinsePEEV.png'
+import electronicConsortiumCover from '../images/electronicConsortium.png'
+import tremplinAstropolisCover from '../images/tremplinAstropolis.png'
+import resonaCover from '../images/resona.png'
+
 const IndexPage = () => {
   return (
-    <Layout pageTitle="IVY ✌️">
-      <li><Link to="/electroLab">Electro Lab tracklist</Link></li>
-      <li><Link to="/digressionsAuditives">Digressions Auditives tracklist</Link></li>
-    </Layout>
+    <div>
+      <div class="titleContainer">
+        <h1 class="title">IVY <span>✌️</span></h1>
+      </div>
+      <div class="bio">
+        <p>Amatrice d’ambiances lourdes et breakées, <b>IVY</b> explore quotidiennement et avec exigence les tréfonds des musiques électroniques tout genre confondu pour n’en ressortir que ce qui la fait vibrer au plus au point. Membre du collectif <a href="https://www.facebook.com/Breakboostaaa" target="_blank"><b>BreakBoost</b></a> à Rennes, aux côtés de ses frères d’armes <a href="https://soundcloud.com/schlossc" target="_blank"><b>Schloss</b></a> et <a href="https://soundcloud.com/mi-ntt" target="_blank"><b>MI:NT</b></a>, elle trouve sa famille de coeur et la volonté de partager ses trésors musicaux.<br/>
+        Les yeux rivés sur les scènes émergentes du globe, elle fait constamment évoluer ses influences pour ne jamais se lasser ou bien encore pour assouvir sa soif d’expériences sonores.</p>
+      </div>
+      <div class="podcasts">
+        <h1>Podcasts</h1>
+        <Link to="/electroLab" class="podcastsLink">
+          <img src={electroLabCover} alt="electroLabCover"/>
+        </Link>
+        <Link to="/module" class="podcastsLink">
+          <img src={moduleCover} alt="moduleCover"/>
+        </Link>
+        <Link to="/rinsePEEV" class="podcastsLink">
+          <img src={rinsePEEVCover} alt="rinsePEEVCover"/>
+        </Link>
+        <Link to="/electronicConsortium" class="podcastsLink">
+          <img src={electronicConsortiumCover} alt="electronicConsortiumCover"/>
+        </Link>
+        <Link to="/tremplinAstropolis" class="podcastsLink">
+          <img src={tremplinAstropolisCover} alt="tremplinAstropolisCover"/>
+        </Link>
+        <Link to="/resona" class="podcastsLink">
+          <img src={resonaCover} alt="resonaCover"/>
+        </Link>
+      </div>
+    </div>
   )
 }
 // Step 3: Export your component
